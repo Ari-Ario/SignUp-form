102c4f51906862d4ed3e6159ed0d8d5320846944
Ari-Ario <xusrew.z@gmail.com>
2023-12-16 19:12:38 +0100

    pages created, button functions finished, styles done

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let currentPage = 1;
 function nextPage() {
+    const mainPage = document.getElementById("all-pages");
+    mainPage.style.minWidth = "400px";
     const allPages = document.getElementById(`page${currentPage}`);
     allPages.style.display = "none";
     currentPage++;
     const newPage = document.getElementById(`page${currentPage}`)
     newPage.style.display = "block";
-    newPage.style.textAlign = "center"
-    newPage.style.backgroundColor= "brown"
+    newPage.style.textAlign = "center";
+    // newPage.style.padding= "0";
+}
+
+function secondPage(){
+    currentPage = 2;
+    const allPages = document.getElementById(`page4`);
+    allPages.style.display = "none";
+    const newPage = document.getElementById(`page${currentPage}`)
+    newPage.style.display = "block";
+    newPage.style.textAlign = "center";
 }
 
 function prevPage() {
@@ -34,3 +45,11 @@ function previewForm() {
     newPage.style.textAlign = "center"
     newPage.style.backgroundColor= "brown"
 }
+
+// function btnMouseOut(btn) {
+//     btn.style.backgroundColor ="blue"
+// }
+
+// function btnMouseOut(btn) {
+//     btn.style.backgroundColor ="#FFF"
+// }
